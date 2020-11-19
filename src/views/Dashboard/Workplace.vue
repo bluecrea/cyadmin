516fd36ed1d9f5881a2bbd7f5f639589d12bd873
xianjie <zzhjsan@gmail.com>
2020-11-19 11:23:11 +0800

    update work place

--- src/views/Dashboard/Workplace.vue
+++ src/views/Dashboard/Workplace.vue
@@ -3,9 +3,34 @@
     <Header-bar bar-name="概览" first-menu="控制台"></Header-bar>
     <div class="panel-body">
       <div class="card-collapse">
-        <a-collapse expand-icon-position="right" :activeKey="[1]" :bordered="false">
-          <a-collapse-panel key="1" header="用户总览">
-            <p>A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.</p>
+        <a-collapse expand-icon-position="right" v-model:activeKey="activeKey" :bordered="false">
+          <a-collapse-panel key="1" header="总体情况">
+            <div class="collapse-content">
+              <div class="scorecard-cell">
+                <dl>
+                  <dt><TeamOutlined /> 用户总数</dt>
+                  <dd>1024</dd>
+                </dl>
+              </div>
+              <div class="scorecard-cell">
+                <dl>
+                  <dt><UsergroupAddOutlined /> 新增用户数</dt>
+                  <dd>1024</dd>
+                </dl>
+              </div>
+              <div class="scorecard-cell">
+                <dl>
+                  <dt><FileTextOutlined /> 菜谱总数</dt>
+                  <dd>1024</dd>
+                </dl>
+              </div>
+              <div class="scorecard-cell">
+                <dl>
+                  <dt><PieChartOutlined /> 积分总消耗</dt>
+                  <dd>1024</dd>
+                </dl>
+              </div>
+            </div>
           </a-collapse-panel>
         </a-collapse>
       </div>
@@ -16,10 +41,20 @@
 <script>
 
 import HeaderBar from '@/components/HeaderBar'
+import { TeamOutlined, UsergroupAddOutlined, FileTextOutlined, PieChartOutlined } from '@ant-design/icons-vue';
 
 export default {
   components: {
-    HeaderBar
+    HeaderBar,
+    TeamOutlined,
+    UsergroupAddOutlined,
+    FileTextOutlined,
+    PieChartOutlined
+  },
+  data() {
+    return {
+      activeKey: [1]
+    }
   }
 };
 </script>
